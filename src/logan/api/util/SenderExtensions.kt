e7b31ddcbb6c737c848a9d3388f3c71839383b25
escape9179 <tr3.logan@gmail.com>
2022-07-09 13:16:45 -0400

    Rename sendColoredMessage to sendMessage

--- src/logan/api/util/SenderExtensions.kt
+++ src/logan/api/util/SenderExtensions.kt
@@ -2,9 +2,13 @@ package logan.api.util
 
 import org.bukkit.ChatColor
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 
-fun CommandSender.sendColoredMessage(message: String) {
-    this.sendMessage(ChatColor.translateAlternateColorCodes('&', message))
+fun CommandSender.sendMessage(message: String, translateColorCodes: Boolean) {
+    sendMessage(
+        if (translateColorCodes) ChatColor.translateAlternateColorCodes('&', message)
+        else message
+    )
 }
 
 fun CommandSender.hasNoPermission(node: String): Boolean {
