7b55ec1262cd535d658e961fb6f829d6c9f5f38b
escape9179 <tr3.logan@gmail.com>
2023-08-02 02:20:58 -0400

    Add color formatting and color validation methods
    This update introduces methods for sending color formatted text and checking validity of Hex and RGB colors. A new method `sendColoredFormattedMessage()` is added to streamline sending color messages. Also, doing Hex to RGB conversion now accounts for Hex strings with a '#' sign. Finally, two new regex based methods, `isHexColor() and `isRgbColor()`, are introduced for checking the validity of color strings. These changes provide better color handling for user input.

--- src/logan/api/util/Functions.kt
+++ src/logan/api/util/Functions.kt
@@ -15,6 +15,10 @@ fun CommandSender.sendMessage(message: String, translateColorCodes: Boolean) {
     )
 }
 
+fun CommandSender.sendColoredFormattedMessage(message: String, vararg params: Any) {
+    sendMessage(String.format(message, params), true)
+}
+
 fun CommandSender.hasNoPermission(node: String): Boolean {
     return !this.hasPermission(node)
 }
@@ -32,7 +36,20 @@ fun Location.toBlockLocation() = Location(world, blockX.toDouble(), blockY.toDou
 fun String.toBukkitColor(): Color {
     return split(" ")
         .run {
-            if (size == 1) Color.fromRGB(Integer.decode(this[0]))
+            if (size == 1) {
+                val formattedHexString = this[0].replace("#", "0x")
+                Color.fromRGB(Integer.decode(formattedHexString))
+            }
             else Color.fromRGB(this[0].toInt(), this[1].toInt(), this[2].toInt())
         }
+}
+
+fun String.isHexColor(): Boolean {
+    val regex = "(#|0x)(\\d|[A-Fa-f]){6}".toRegex()
+    return this.matches(regex)
+}
+
+fun String.isRgbColor(): Boolean {
+    val regex = "\\d{1,3} \\d{1,3} \\d{1,3}".toRegex()
+    return this.matches(regex)
 }
